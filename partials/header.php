4a9d89420bc8817cc61df6811f1c0fae6607277a
abbasnake <mr.rihards.ozols@gmail.com>
2018-02-11 17:55:54 +0100

    deleted old files

--- partials/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>test</title>
-    <link href="https://fonts.googleapis.com/css?family=Reenie+Beanie" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div class="grid">
